1b83c7396e7b58927c45e8b3c33b86f8fe116f13
Mark Deuerling <deuerlingmark@yahoo.de>
2017-10-14 13:48:39 +0200

    small changes

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class Extensions
+{ 
+    public static Rigidbody2D GetRigidbody(this MonoBehaviour physics)
+    {
+        return physics.GetComponent<Rigidbody2D>();
+    }
+}
