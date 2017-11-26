b3306c70ef245af28f1ba28149c6c6df3c0ab732
Mark Deuerling <deuerlingmark@yahoo.de>
2017-11-26 23:55:38 +0100

    add moving powerups

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float DestroyTime;
+
+    private void Start()
+    {
+        Destroy(gameObject, DestroyTime);
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.right * MoveSpeed * Time.deltaTime;
+    }
+}
