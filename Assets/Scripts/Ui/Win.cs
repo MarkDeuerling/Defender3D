e9155b656e813e9495cde22b19bf2aa68148f644
MarkDeuerling <deuerlingmark@yahoo.de>
2018-01-12 00:43:14 +0100

    adjust scenes

---
+++ Assets/Scripts/Ui/Win.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+using UnityEngine.SceneManagement;
+
+
+public class Win : MonoBehaviour 
+{
+	public void OnTitle() 
+	{
+		SceneManager.LoadScene("Start");
+		SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+		SceneManager.LoadScene("Camera", LoadSceneMode.Additive);
+	}
+}
