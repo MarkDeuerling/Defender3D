43b802e3e9e038ba38d0f5a3728ed41f9ca80006
MarkDeuerling <deuerlingmark@yahoo.de>
2018-01-21 15:22:57 +0100

    fixed build

---
+++ Assets/First.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class First : MonoBehaviour 
+{
+    private void Start()
+    {
+        SceneManager.LoadScene("Start");
+        SceneManager.LoadScene("Camera", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+    }
+}
--- Assets/Scripts/Transition/BossTransition.cs
+++ Assets/Scripts/Transition/BossTransition.cs
@@ -9,32 +9,30 @@ namespace Transition
         public float MaxBlur;
         public float BlurFactor;
         public float CleanUpTime;
-
         public GameObject Boss;
+
+        private PostProcessingCamera ppC;
 
         private void Start()
         {
+            ppC = Camera.main.GetComponent<PostProcessingCamera>();
             StartCoroutine(Transition());
         }
 
         private IEnumerator Transition()
         {
             yield return new WaitForSeconds(CleanUpTime);
-            GetPpCam().UseBlur = true;
-            GetPpCam().BlurConfig.Blur = 0;
-            while (GetPpCam().BlurConfig.Blur < MaxBlur)
+            ppC.UseBlur = true;
+            ppC.BlurConfig.Blur = 0;
+            
+            while (ppC.BlurConfig.Blur < MaxBlur)
             {
-                GetPpCam().BlurConfig.Blur += BlurFactor * Time.deltaTime;
+                ppC.BlurConfig.Blur += BlurFactor * Time.deltaTime;
                 yield return new WaitForSeconds(0.02f);
             }
             Instantiate(Boss, transform.position, transform.rotation);
             yield return new WaitForSeconds(.2f);
-            GetPpCam().UseBlur = false;
-        }
-
-        private static PostProcessingCamera GetPpCam()
-        {
-            return Camera.main.GetComponent<PostProcessingCamera>();
+            ppC.UseBlur = false;
         }
     }
 }
